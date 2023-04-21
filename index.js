0239a9b6a09f932fdde0a212811aa266b8f9ddf5
Ethan <ethan.nordahl1@gmail.com>
2023-04-21 02:28:11 -0700

    done

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ async function searchMovies(searchTerm) {
   await timeout(1000);
   spinnerEl.remove();
   moviesListEl.innerHTML = moviesData.Search.map(movie => movieHTML(movie)).join('');
-  console.log(moviesListEl)
   document.getElementById('filter').selectedIndex = 0;
 }
 
