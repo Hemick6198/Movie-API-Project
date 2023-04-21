cb429df289411e7ae5e7a6dbb6c71c5612db93dc
Ethan <ethan.nordahl1@gmail.com>
2023-04-20 20:52:04 -0700

    somehow got the sort function working

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 // http://www.omdbapi.com/?i=tt3896198&apikey=23587054
-
-const moviesListEl = document.querySelector('.movies');
 const API_KEY = '23587054';
+let moviesListEl = document.querySelector('.movies');
+let movies;
+let moviesData = {};
 
 async function main(searchTerm) {
-    try {
-      const movies = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchTerm}`);
-      if (!movies.ok) {
-        throw new Error('Failed to fetch movies from the API.');
-      }
-      const moviesData = await movies.json();
-      console.log(moviesData);
-      moviesListEl.innerHTML = moviesData.Search.map(movie => movieHTML(movie)).join('');
-    } catch (error) {
-      console.error(error);
-    }
+            const movies = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchTerm}`);
+            const moviesData = await movies.json();
+            moviesListEl.innerHTML = moviesData.Search.map(movie => movieHTML(movie)).join('');
 }
 
 const searchFormEl = document.querySelector('.search-form');
@@ -24,6 +17,35 @@ searchFormEl.addEventListener('submit', (event) => {
     const searchTerm = searchInputEl.value;
     main(searchTerm);
 });
+
+
+
+async function sortMovies(filter) {
+  const moviesWrapper = document.querySelector(`.movies`)
+  // const moviesData = {};
+  moviesWrapper.classList += ' movies__loading'
+  
+    if (!moviesData) {
+        movies = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchTerm}`);
+        // const moviesData = await movies.json();
+    }
+    // console.log(moviesData)
+    moviesWrapper.classList.remove('movies__loading')
+
+    if (filter === 'OLDEST') {
+        moviesData.Search.sort((a, b) => a.Year - b.Year);
+        moviesListEl.innerHTML = moviesData.Search.map(movie => movieHTML(movie)).join('');
+      }
+    else if (filter === 'NEWEST') {
+        moviesData.Search.sort((a, b) => b.Year - a.Year);
+        moviesListEl.innerHTML = moviesData.Search.map(movie => movieHTML(movie)).join('');
+      }
+}
+
+function filter(event) {
+    sortMovies(event.target.value);
+}
+
 
 function movieHTML(movie) {
     return `
@@ -45,6 +67,7 @@ function movieHTML(movie) {
       </div>
     `;
 }
+
 
 function openMenu() {
     document.body.classList += "menu--open"
